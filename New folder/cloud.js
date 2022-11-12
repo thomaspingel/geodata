3106718eca89aea1529aa9920c6d5e87da490b32
Thomas Pingel <thomas.pingel@gmail.com>
2022-11-11 20:15:29 -0500

    tree

---
+++ New folder/cloud.js
@@ -0,0 +1,30 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 3007411,
+    "boundingBox": {
+        "lx": 553824.6710205078,
+        "ly": 4118274.3649902346,
+        "lz": 594.4310302734375,
+        "ux": 553848.8460083008,
+        "uy": 4118298.5399780275,
+        "uz": 618.6060180664063
+    },
+    "tightBoundingBox": {
+        "lx": 553824.6710205078,
+        "ly": 4118274.3649902346,
+        "lz": 594.4310302734375,
+        "ux": 553844.6740205078,
+        "uy": 4118293.8769902347,
+        "uz": 618.6060302734375
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 0.20936153829097749,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
