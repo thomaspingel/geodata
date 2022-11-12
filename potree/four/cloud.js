c288a6bbdd3c0e0023446814a516fbeeea71f889
Thomas Pingel <thomas.pingel@gmail.com>
2022-11-11 20:19:13 -0500

    better

---
+++ potree/four/cloud.js
@@ -0,0 +1,75 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 4,
+    "boundingBox": {
+        "lx": 0.0,
+        "ly": 0.0,
+        "lz": 0.0,
+        "ux": 1.0,
+        "uy": 1.0,
+        "uz": 1.0
+    },
+    "tightBoundingBox": {
+        "lx": 0.0,
+        "ly": 0.0,
+        "lz": 0.0,
+        "ux": 1.0,
+        "uy": 1.0,
+        "uz": 1.0
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.008660254068672657,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
